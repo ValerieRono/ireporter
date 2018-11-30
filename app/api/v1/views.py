36f8041777f809defd4c76a5690825dd44d4fc4f
valerie <ronovalerie@gmail.com>
2018-11-30 09:50:25 +0300

    add post method to resource

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -14,3 +14,26 @@ class MyIncidents(Resource):
     def get(self):
         incidents = incidents_Schema.dump(incident_list).data
         return {"status":"success", "data":incidents}, 200
+    
+    def post(self):
+        json_data = request.get_json(force=True)
+        if not json_data:
+               return {'message': 'No input data provided'}, 400
+        # Validate and deserialize input
+        data, errors = incident_Schema.dump(json_data)
+        if errors:
+            return {"status": "error", "data": errors}, 422
+        new_incident = Incident(
+            createdBy = data['createdBy'],
+            type_of_incident = data['type_of_incident'],
+            location = data['location'],
+            status = data['status'],
+            images = data['images'],
+            videos = data['videos'],
+            comment = data['comment']
+            )
+        
+        incident_list.append(new_incident)
+        result = incident_Schema.dump(new_incident).data
+
+        return {'status': "success", 'data': result}, 201
