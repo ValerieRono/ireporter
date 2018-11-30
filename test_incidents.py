00ae03807800edcfc7b2e55aab758dd9a8f87b2e
valerie <ronovalerie@gmail.com>
2018-12-01 00:55:45 +0300

    create tests file, add tests

---
+++ test_incidents.py
@@ -0,0 +1,83 @@
+"""Test for methods applied to incident records"""
+import json
+import os
+import unittest
+import marshmallow
+
+from app.api.v1.views import incident_Schema, incident_list
+
+#local import 
+from base import BaseTestCase
+
+class TestRequests(BaseTestCase):
+    """Tests"""
+    def test_new_incident(self):
+        """Test for posting an incident"""
+        #correct request
+        response = self.client.post('api/v1/incidents', data=json.dumps(
+            self.incident), headers={'content-type': "application/json"})
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result['data'][0]["message"], 'created red flag record')
+
+    def test_new_incident_without_comment(self):
+        """Test for posting a redflag without a comment"""
+        #no body
+        response = self.client.post('api/v1/incidents', data=json.dumps(
+            self.incident_without_comment), headers={'content-type': "application/json"})
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_all_incidents(self):
+        """Test for viewing all redflags"""
+        response = self.client.get('api/v1/incidents')
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result['data'][0]["message"], 'successfully fetched all records')
+        
+        
+
+    def test_get_specific_incident(self):
+        """Test for viewing a particular redflag"""
+        #existing redflag
+        response = self.client.get('api/v1/incidents/1')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_incident_not_found(self):
+        """Test for viewing a redflag that does not exist"""
+        #redflag does not exist
+        response = self.client.get('api/v1/incidents/20')
+        self.assertEqual(response.status_code, 404)
+
+
+    def test_edit_an_incident(self): 
+        """Test for modifying a redflag """
+        response = self.client.put('api/v1/incidents/2',
+                                data=json.dumps(self.update_incident),
+                                headers={'content-type': "application/json"})
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result['data'][0]["message"], 'updated red flag record')
+
+        
+    def test_user_delete_incident(self):
+        """Test for deleting a redflag"""
+        response = self.client.delete('api/v1/incidents/2')
+        self.assertEqual(response.status_code, 200)
+        result = json.loads(response.data)
+        self.assertEqual(result['data'][0]["message"], 'deleted a red flag record')
+
+    def test_no_input(self):
+        response1 = self.client.post('api/v1/incidents',
+                                data=json.dumps(self.no_input),
+                                headers={'content-type': "application/json"})
+        response2 = self.client.put('api/v1/incidents/2',
+                                data=json.dumps(self.no_input),
+                                headers={'content-type': "application/json"})
+        self.assertEqual(response1.status_code, 200)
+        result1 = json.loads(response1.data)
+        self.assertEqual(result1["message"], 'no input data provided')
+        self.assertEqual(response2.status_code, 200)
+        result2 = json.loads(response2.data)
+        self.assertEqual(result2["message"], 'no input data provided')
+
+        
