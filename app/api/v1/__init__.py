d5f870856186f4bc42287171a9f692e4fc64d442
valerie <ronovalerie@gmail.com>
2018-11-30 09:33:08 +0300

    set up API resource routing

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api, Resource 
 
 #local import
+from views import MyIncidents, MyIncident
 
 
 v1 = Blueprint('api-v1', __name__, url_prefix='/api/v1')
 api = Api(v1)
 
 ## setup API resource routing
+api.add_resource(MyIncidents, '/incidents', endpoint='incidents')
