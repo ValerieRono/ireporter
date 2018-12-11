b1dd9fa9319690c07844f9701acb02e488d7b701
valerie <ronovalerie@gmail.com>
2018-12-11 13:01:46 +0300

    fixed pep8 issues and put method

--- app/api/v1/models.py
+++ app/api/v1/models.py
@@ -1,7 +1,5 @@
-from flask import Flask
 from flask_restful import fields, reqparse, inputs
 import datetime
-import re
 
 incidents = []
 
@@ -11,8 +9,9 @@ def generateId():
     return 1
 
 class Incidents():
-    def __init__(self, createdBy, type_of_incident, location, 
-                    images, videos, comment):
+    def __init__(
+        self, createdBy, type_of_incident, location, images, videos, comment
+    ):
         self.id = generateId()
         self.createdOn = datetime.datetime.now()
         self.createdBy = createdBy
@@ -24,20 +23,49 @@ class Incidents():
         self.comment = comment
 
 record_parser = reqparse.RequestParser()
-record_parser.add_argument('createdBy', required=True, help='please provide input', type=str, location='json')
-record_parser.add_argument('type_of_incident', required=True, help='please provide input', type=str, default='', location='json')
-record_parser.add_argument('location', required=True, help='please provide input', type=str, location='json')
-record_parser.add_argument('images', required=True, help='please provide input', type=str, location='json')
-record_parser.add_argument('videos', required=True, help='please provide input', type=str, default='', location='json')
-record_parser.add_argument('comment', required=True, help='please comment', type=str, location='json')
+
+record_parser.add_argument(
+    'createdBy', required=True, help='provide input', type=str, location='json'
+    )
+record_parser.add_argument(
+    'type_of_incident', required=True,
+    help='can only be Redflag or Intervention',
+    type=inputs.regex(r'^\b(Redflag|Intervention)\b$'), location='json'
+    )
+record_parser.add_argument(
+    'location', required=True, help='provide input', type=str, location='json'
+    )
+record_parser.add_argument(
+    'images', required=True, help='provide input', type=str, location='json'
+    )
+record_parser.add_argument(
+    'videos', required=True, help='provide input', type=str, location='json'
+    )
+record_parser.add_argument(
+    'comment', required=True, help='please comment', type=str, location='json'
+    )
 
 edit_parser = reqparse.RequestParser()
-edit_parser.add_argument('createdBy', type=str, location='json')
-edit_parser.add_argument('type_of_incident', type=str, default='', location='json')
-edit_parser.add_argument('location', type=str, location='json')
-edit_parser.add_argument('images', type=str, location='json')
-edit_parser.add_argument('videos', type=str, default='', location='json')
-edit_parser.add_argument('comment', type=str, location='json')
+
+edit_parser.add_argument(
+    'createdBy', type=str, location='json'
+    )
+edit_parser.add_argument(
+    'type_of_incident',
+    type=inputs.regex(r'^\b(Redflag|Intervention)\b$'), location='json'
+    )
+edit_parser.add_argument(
+    'location', type=str, location='json'
+    )
+edit_parser.add_argument(
+    'images', type=str, location='json'
+    )
+edit_parser.add_argument(
+    'videos', type=str, default='', location='json'
+    )
+edit_parser.add_argument(
+    'comment', type=str, location='json'
+    )
 
 record_fields = {
     "id": fields.Integer,
--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,9 +1,9 @@
-from flask import Flask, json, jsonify, abort, make_response
-from flask_restful import Api, Resource, reqparse, marshal
+from flask_restful import Resource, marshal
 from marshmallow import Schema, fields
 
 # local import
-from app.api.v1.models import record_fields, incidents, record_parser, Incidents, edit_parser
+from app.api.v1.models import record_fields, incidents
+from app.api.v1.models import record_parser, Incidents, edit_parser
 
 # for serialization 
 class IncidentSchema(Schema):
@@ -21,38 +21,49 @@ incident_Schema = IncidentSchema()
 incidents_Schema = IncidentSchema(many=True)
 
 class MyIncidents(Resource):
+
     def __init__(self):
         super(MyIncidents, self).__init__()
         self.parser = record_parser
 
     def get(self):
         if len(incidents) == 0:
-            return {"status": 404, "data": [{"message": "no records found"}]}, 404
+            return {
+                "status": 404,
+                "data": [{"message": "no records found"}]
+                }, 404
         result = [marshal(incident, record_fields) for incident in incidents]
-        return {"status": 200, "data": [{"incidents": result, "message": "successfully fetched all records"}]}, 200
-        
+        return {
+            "status": 200,
+            "data": [{"incidents": result, "message": "successfull"}]
+            }, 200
 
     def post(self):
         args = self.parser.parse_args()
         keys = args.keys()
         for key in keys:
             if not args[key]:
-                return {"status": 400, "data": [{"message": "bad request"}]}, 400
+                return {
+                    "status": 400,
+                    "data": [{"message": "bad request"}]
+                    }, 400
         incident = Incidents(
-            createdBy = args['createdBy'],
-            type_of_incident = args['type_of_incident'],
-            location = args['location'],
-            images = args['images'],
-            videos = args['videos'],
-            comment = args['comment']
+            createdBy=args['createdBy'],
+            type_of_incident=args['type_of_incident'],
+            location=args['location'],
+            images=args['images'],
+            videos=args['videos'],
+            comment=args['comment']
         )
         result = marshal(incident, record_fields)
         incidents.append(result)
-        return {'status': 201, 'data': [{"record": result, "message": "created red flag record"}]}, 201
-        
-
+        return {
+            'status': 201,
+            'data': [{"record": result, "message": "created record"}]
+            }, 201
 
 class MyIncident(Resource):
+
     def __init__(self):
         super(MyIncident, self).__init__()
         self.parser = record_parser
@@ -60,40 +71,61 @@ class MyIncident(Resource):
     def get(self, id):
         new_incidents = incidents_Schema.dump(incidents).data
         incident = [incident for incident in new_incidents if incident['id'] == id]
+
         if len(incident) == 0:
-            return {"status": 404, "data": [{"message": "record not found"}]}, 404
+            return {
+                "status": 404, 'data': [{"message": "record not found"}]
+                }, 404
         result = marshal(incident[0], record_fields)
-        return {"status": 200, "data": [{"incident": result, "message": "successfully fetched record"}]}, 200
-        
+        return {
+            "status": 200,
+            "data": [{"incident": result, "message": "successfull"}]
+            }, 200
+
     def put(self, id):
         new_incidents = incidents_Schema.dump(incidents).data
         incident = [incident for incident in new_incidents if incident['id'] == id]
+
         if len(incident) == 0:
-            return {"status": 404, "data": [{"message": "record not found"}]}, 404
+            return {
+                "status": 404,
+                "data": [{"message": "record not found"}]
+                }, 404
+        incident_index = new_incidents.index(incident[0])
+
         if incident[0]['status'] != "draft":
-                    return {'status': 400, 'data': [{"message" : "cannot edit record"}]}, 400
+            return {
+                'status': 400,
+                'data': [{"message": "cannot edit record"}]
+                }, 400
 
-        args = edit_parser.parse_args()
-        for key in args.keys():
-            if args[key]:
-                incident[0][key] = args[key]        
+        data = edit_parser.parse_args()
+        for key in data.keys():
+            if data[key]:
+                incident[0][key] = data[key]        
 
-        updated_incident = incident_Schema.load(incident[0]).data
-        incidents[id-1] = updated_incident
-
-        result = incident_Schema.dump(incident[0]).data
-        return {'status': 200, 'data': [{'record': marshal(result, record_fields), "message": "updated red flag record"}]}, 200
+        incidents[incident_index] = incident[0]
+        return {
+            'status': 200,
+            'data': [{"record": incident[0], "message": "updated record"}]
+            }, 200
 
     def delete(self, id):
         new_incidents = incidents_Schema.dump(incidents).data
         incident = [incident for incident in new_incidents if incident['id'] == id]
+
         if len(incident) == 0:
-            return {"status": 404, "data": [{"message": "record not found"}]}, 404
+            return {
+                "status": 404,
+                "data": [{"message": "record not found"}]
+                }, 404
+
         for item in incidents:
             if incident_Schema.dump(item).data == incident[0]:
                 incidents.remove(item)
-        return {'status': 200, 'data': [{'record': marshal(incident[0], record_fields), "message": "deleted a red flag record"}]}, 200
-        
-       
-        
-        
+        result = marshal(incident[0], record_fields)
+
+        return {
+            'status': 200,
+            'data': [{'record': result, "message": "deleted"}]
+            }, 200
